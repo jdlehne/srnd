d9d0fa66309653cf3d25d0f21b3ba6748abcf621
Josh Lehne <jdlehne@gmail.com>
2017-10-09 09:34:07 -0500

    finished add with allowing empty values

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -20,6 +20,14 @@ module.exports = function(app) {
       added_by: req.body.added_by,
       ingredient_1: req.body.ingredient_1,
       ing_qty_1: req.body.ing_qty_1,
+      ingredient_2: req.body.ingredient_2,
+      ing_qty_2: req.body.ing_qty_2,
+      ingredient_3: req.body.ingredient_3,
+      ing_qty_3: req.body.ing_qty_3,
+      ingredient_4: req.body.ingredient_4,
+      ing_qty_4: req.body.ing_qty_4,
+      ingredient_5: req.body.ingredient_5,
+      ing_qty_5: req.body.ing_qty_5,
       description: req.body.description,
     }).then(function(dbDrinks) {
       res.json(dbDrinks);
