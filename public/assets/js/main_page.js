9dfe7fda61f9d23ff250ff2cd752e9cee3dea679
Josh Lehne <jdlehne@gmail.com>
2017-10-09 08:27:46 -0500

    drink json capture

--- public/assets/js/main_page.js
+++ public/assets/js/main_page.js
@@ -1,3 +1,5 @@
+$(document).on("submit", "#addForm", addDrink);
+
 var ingred = 1;
 
 function ingredient_field() {
@@ -8,7 +10,7 @@ function ingredient_field() {
     var added_ingredient = document.createElement("div");
     added_ingredient.setAttribute("id", "addedIng" + ingred);
     var rdiv = 'addedIng' + ingred;
-    added_ingredient.innerHTML = '<div class="col-offset-2"></div><div class="form-group"><label class="col-xs-2 control-label">Ingredient</label><div class="col-xs-5"><input type="text" class="form-control" id="ingredName" name="ingredName[]" value="" placeholder="Ingredient"></div><div class="col-xs-2"><input type="text" class="form-control" id="qty" name="qty[]" value="" placeholder="qty"></div><div class="input-group"><div class="input-group-btn"><button class="btn btn-danger" type="button" onclick="remove_ingredient_field(' + ingred + ');"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button></div></div></div>';
+    added_ingredient.innerHTML = '<div class="col-offset-2"></div><div class="form-group"><label class="col-xs-2 control-label">Ingredient</label><div class="col-xs-5"><input type="text" class="form-control" id="ingredName" name="ingredName[]" value="" placeholder="Ingredient"></div><div class="col-xs-2"><input type="text" class="form-control" id="qty" name="qty[]" value="" placeholder="quantity in oz"></div><div class="input-group"><div class="input-group-btn"><button class="btn btn-danger" type="button" onclick="remove_ingredient_field(' + ingred + ');"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button></div></div></div>';
 
     objTo.appendChild(added_ingredient);
   }
@@ -17,4 +19,21 @@ function ingredient_field() {
 
 function remove_ingredient_field(rid) {
   $('#addedIng' + rid).remove();
+}
+
+
+
+function addDrink(event) {
+  console.log("Adding Drink");
+  event.preventDefault();
+  var drink = {
+    drink_name: $('#drinkName').val().trim(),
+     added_by: $('#addedBy').val().trim(),
+     indredient_1: $('#ingredName').val().trim(),
+     ing_qty_1: $('#qty').val().trim(),
+     description: $('#description').val().trim(),
+  };
+
+  $.post("/api/drinks", drink);
+  console.log(drink);
 }
